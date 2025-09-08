97a002d47d00cb2e2ae632558c8043cee018cac1
berdina67 <berdina67@gmail.com>
2025-09-08 10:47:51 -0300

    configurção de mensagens de alerta(bootstrap)

--- core/settings.py
+++ core/settings.py
@@ -201,6 +201,17 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
+# Configuração de Messages (Class Boostrap) #
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+	constants.ERROR: 'alert-danger',
+	constants.WARNING: 'alert-warning',
+	constants.DEBUG: 'alert-danger',
+	constants.SUCCESS: 'alert-success',
+	constants.INFO: 'alert-info',
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
